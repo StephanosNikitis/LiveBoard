ba231ff8b51c99f085b2b30e293690ce5ff64fc2
Shaurya <theshaurya035@gmail.com>
2026-03-19 22:31:56 +0530

    Added delete layer feature

--- app/board/[boardId]/_components/selection-tools.tsx
+++ app/board/[boardId]/_components/selection-tools.tsx
@@ -1,10 +1,14 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
+import { Hint } from "@/components/hint";
+import { Trash2 } from "lucide-react";
 import { useSelectionBounds } from "@/hooks/use-selection-bounds";
 import { useMutation, useSelf } from "@/liveblocks.config";
 import { Camera, Color } from "@/types/canvas";
 import { memo } from "react";
 import { ColorPicker } from "./color-picker";
+import { useDeleteLayers } from "@/hooks/use-delete-layers";
 
 interface SelectionToolsProps {
     camera: Camera;
@@ -29,6 +33,8 @@ export const SelectionTools = memo(({
         })
     }, [selection, setLastUsedColor]);
 
+    const deleteLayers = useDeleteLayers();
+
     const selectionBounds = useSelectionBounds();
 
     if(!selectionBounds) {
@@ -51,6 +57,17 @@ export const SelectionTools = memo(({
             <ColorPicker 
                 onChange={setFill}
             />
+            <div className="flex items-center pl-2 ml-2 border-l border-neutral-200">
+                <Hint label="Delete">
+                    <Button
+                        variant="board"
+                        size="icon"
+                        onClick={deleteLayers}
+                    >
+                        <Trash2 />
+                    </Button>
+                </Hint>
+            </div>
         </div>
     );
 });
