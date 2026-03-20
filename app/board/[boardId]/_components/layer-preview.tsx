bb2d810876406bcebc21bb90f38894123964440d
Shaurya <theshaurya035@gmail.com>
2026-03-21 01:13:00 +0530

    Added Text and Note Layer

--- app/board/[boardId]/_components/layer-preview.tsx
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -5,6 +5,8 @@ import { LayerType } from "@/types/canvas";
 import { memo } from "react";
 import { Rectangle } from "./rectangle";
 import { Ellipse } from "./ellipse";
+import { Text } from "./text";
+import { Note } from "./note";
 
 interface LayerPreviewProps {
     id: string;
@@ -40,6 +42,24 @@ export const LayerPreview = memo(({
                     layer={layer}
                     onPointerDown={onLayerPointerDown}
                     selectionColor={selectionColor} 
+                />
+            );
+        case LayerType.Text:
+            return (
+                <Text 
+                    id={id}
+                    layer={layer}
+                    onPointerDown={onLayerPointerDown}
+                    selectionColor={selectionColor}
+                />
+            );
+        case LayerType.Note:
+            return (
+                <Note 
+                    id={id}
+                    layer={layer}
+                    onPointerDown={onLayerPointerDown}
+                    selectionColor={selectionColor}
                 />
             );
         default:
